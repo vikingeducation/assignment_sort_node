aaa06d9da9963787286a53c9aa9bf3965e7681b7
William Whitworth <william10charles@gmail.com>
2017-06-06 10:07:36 -0600

    starting merge

---
+++ mergeSort.js
@@ -0,0 +1,12 @@
+const merge = (arr1, arr2) => {
+  //
+};
+
+const mergeSort = arr => {
+  //
+};
+
+console.log(mergeSort([9, 8, 7, 4, 4, 4, 3, 2, 1]));
+console.log(mergeSort([9, 8, 7, 6, 5, 4, 3, 2, 1]));
+console.log(mergeSort([3, 1, 6, 2, 7, 4, 5]));
+console.log(mergeSort([3, -1, 6, 2, -7, 4, 5]));
