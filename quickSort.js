2c7422550e92b0f9db80bfe350c9ed28c7da48f9
Egle Libby <eglelibby@gmail.com>
2017-06-06 13:34:03 -0400

    quicksort

--- quickSort.js
+++ quickSort.js
@@ -1,21 +1,30 @@
 const partition = (arr, pivot, rightMost) => {
+  let resultArr = [arr[pivot]];
+  let pivotPos = 0;
   for (let i = 0; i < rightMost; i++) {
-    if (arr[i] > arr[pivot] && i < pivot) {
+    if (arr[i] > arr[pivot]) {
       //
+      resultArr.push(arr[i]);
+    } else {
+      resultArr.unshift(arr[i]);
+      pivotPos++;
     }
   }
+  return pivotPos;
 };
 
 const quickSort = (arr, pivot = arr.length - 1, rightMost = arr.length - 1) => {
   //
   if (pivot < rightMost) {
-    var q = partition(arr, pivot, rightMost);
-    quickSort(arr, pivot, q - 1);
-    quickSort(arr, q, rightMost);
+    var correctPos = partition(arr, pivot, rightMost);
+    quickSort(arr, pivot, correctPos - 1);
+    quickSort(arr, correctPos + 1, rightMost);
   }
+  return arr;
 };
-
-console.log(quickSort([9, 8, 7, 4, 4, 4, 3, 2, 1]));
+let arr = [9, 8, 7, 4, 4, 4, 3, 2, 1];
+quickSort(arr);
+console.log(arr);
 console.log(quickSort([9, 8, 7, 6, 5, 4, 3, 2, 1]));
 console.log(quickSort([3, 1, 6, 2, 7, 4, 5]));
 console.log(quickSort([3, -1, 6, 2, -7, 4, 5]));
