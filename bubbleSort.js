149530cab0bd88c2e7f5e740c49734b41836f6a1
Eric Glover <etgt28@mail.missouri.edu>
2017-09-26 11:03:13 -0500

    bubbling with the sorts

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,11 +1,25 @@
 //grab first thing in unsorted arr, insert appropriately in sorted arr
+//shifts in place
 const bubbleSort = arr => {
-  return null;
+  //start at left, grab highest, place at end, swapping along the way
+  for (let i = 0; i < arr.length; i++) {
+    //arr.length - i as the inner loop condition, because the right side is our sorted arr
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] <= arr[j + 1]) {
+        //do nothing
+      } else if (arr[j] > arr[j + 1]) {
+        //swap
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
 };
 
 const test = () => {
   console.log(`sorting ${[1, 3, 7, 2, 5]} should output ${[1, 2, 3, 5, 7]} `);
   console.log(bubbleSort([1, 3, 7, 2, 5]));
-  // console.log(insertInPlace([1, 2, 3], 7, 1));
 };
 test();
