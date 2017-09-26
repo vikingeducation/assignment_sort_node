8ed616e765558baf2155c292a24fb758d15c3fbe
Eric Glover <etgt28@mail.missouri.edu>
2017-09-26 10:57:55 -0500

    insertion sorting

---
+++ bubbleSort.js
@@ -0,0 +1,11 @@
+//grab first thing in unsorted arr, insert appropriately in sorted arr
+const bubbleSort = arr => {
+  return null;
+};
+
+const test = () => {
+  console.log(`sorting ${[1, 3, 7, 2, 5]} should output ${[1, 2, 3, 5, 7]} `);
+  console.log(bubbleSort([1, 3, 7, 2, 5]));
+  // console.log(insertInPlace([1, 2, 3], 7, 1));
+};
+test();
